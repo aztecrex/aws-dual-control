fc6a5d93ab23de91231ca71bf05038a655011686
Greg Wiley <gwiley@cj.com>
2018-05-12 09:47:41 -0700

    require a reason

--- test/Spec/DualControl.hs
+++ test/Spec/DualControl.hs
@@ -4,6 +4,7 @@ import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=), Assertion)
 
 import Data.Text (Text)
+import qualified Data.Text as T (length)
 
 (===) :: (Eq a, Show a) => a -> a -> Assertion
 (===) = (@?=)
@@ -20,7 +21,7 @@ tests = testGroup "DualControl" [
             principals = [principal1, principal2]
 
         -- when
-            actual = vend principals
+            actual = grant principals "say so"
 
         -- then
         run token actual === Just token,
@@ -35,7 +36,7 @@ tests = testGroup "DualControl" [
             principals = [principal1, principal2, principal3]
 
         -- when
-            actual = vend principals
+            actual = grant principals "crash"
 
         -- then
         run token actual === Just token,
@@ -48,7 +49,31 @@ tests = testGroup "DualControl" [
             principals = [principal]
 
         -- when
-            actual = vend principals
+            actual = grant principals "overload"
+
+        -- then
+        run token actual === Nothing,
+
+    testCase "grant when reason provided" $ do
+        let
+        -- given
+            token = "acc"
+            reason = "on fire"
+
+        -- when
+            actual = grant ["percival", "nona"] reason
+
+        -- then
+        run token actual === Just token,
+
+    testCase "deny grant when reason is empty" $ do
+        let
+        -- given
+            token = "cccca"
+            reason = ""
+
+        -- when
+            actual = grant ["sheila", "thomas"]  reason
 
         -- then
         run token actual === Nothing
@@ -59,9 +84,9 @@ tests = testGroup "DualControl" [
 run :: Text -> (Text -> Maybe Text) -> Maybe Text
 run = flip ($)
 
-vend :: [Text] -> Text -> Maybe Text
-vend principals | length principals >= 2 = Just
-                | otherwise = const Nothing
+grant :: [Text] -> Text -> Text -> Maybe Text
+grant principals reason  | T.length reason > 0 && length principals >= 2 = Just
+                        | otherwise = const Nothing
 
 
 
